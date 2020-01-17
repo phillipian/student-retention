8efb815cf2fa8757d2091d040854f8e00f4b3dc7
createandbuild <33373263+createandbuild@users.noreply.github.com>
2020-01-16 19:08:48 -0500

    senior class graph added

--- src/JS/graph.js
+++ src/JS/graph.js
@@ -1,57 +1,163 @@
-var chart = Highcharts.chart('line-graph', {
-    chart: {
-        type: 'area',
-        style: {
-          fontFamily: 'Gotham',
-          fontSize: '15px'
-        },
-        backgroundColor: "#ECF0F1"
+var chart = Highcharts.chart('senior-class', {
+  chart: {
+    type: 'area',
+    style: {
+      fontFamily: "sans-serif",
+      fontSize: '15px'
     },
+    backgroundColor: "#ECF0F1"
+  },
+  title: {
+    text: 'Senior Class Sizes Including PGs through the Years',
+    style: {
+      fontWeight: 'bold'
+    }
+  },
+  subtitle: {
+    text: '<i>Note: merged with Abbot Academy in 1973-1974; the data is based on the directory (two members of the Class of 2020 have left since)</i>'
+  },
+  legend: {
+    enabled: false
+  },
+  credits: {
+    enabled: false
+  },
+  tooltip: {
+    formatter: function() {
+      return '<b>' + this.x + '</b><br>' +
+        this.series.name + ': <b>' + this.y + '</b>';
+    }
+  },
+  xAxis: {
     title: {
-        text: 'What was your rounded Winter 2018-2019 (unweighted) G.P.A.?',
+        text: 'Graduating Years',
         style: {
+          color: 'black',
           fontWeight: 'bold'
         }
     },
-    subtitle: {
-      text: '<i>Note: If respondents were between two numbers, they were instructed to round up</i><br><i>to the next closest decimal — a 5.3 G.P.A. would round to a 5.4</i>'
+    categories: [1972,
+      1973,
+      1974,
+      1975,
+      1976,
+      1977,
+      1978,
+      1979,
+      1980,
+      1981,
+      1982,
+      1983,
+      1984,
+      1985,
+      1986,
+      1987,
+      1988,
+      1989,
+      1990,
+      1991,
+      1992,
+      1993,
+      1994,
+      1995,
+      1996,
+      1997,
+      1998,
+      1999,
+      2000,
+      2001,
+      2002,
+      2003,
+      2004,
+      2005,
+      2006,
+      2007,
+      2008,
+      2009,
+      2010,
+      2011,
+      2012,
+      2013,
+      2014,
+      2015,
+      2016,
+      2017,
+      2018,
+      2019,
+      2020],
+  },
+  yAxis: {
+    title: {
+      text: 'Size of the Senior and PG Class',
+      style: {
+        color: 'black',
+        fontWeight: 'bold'
+      }
+    }
+  },
+  plotOptions: {
+    area: {
+      fillOpacity: 0.5,
+      color: "#393e85"
     },
-    legend: {
-        enabled: false
-    },
-    tooltip: {
-        formatter: function () {
-            return '<b>' + this.x + '</b><br>' +
-                this.series.name + ': <b>' + this.y + '</b><br>'  + 'Percentage: <b>' + (this.y/10.09).toFixed(1) + '%</b>';
-        }
-    },
-    xAxis: {
-        categories: ['6.0', '5.8', '5.6', '5.4', '5.2', '5.0', '4.8', '4.6', '4.4', '4.2', '4.0', '3.8', '3.6', '3.4', '3.2', '3.0', '2,8', '2.6', '2.4', '2.2', '<=2.0'],
-    },
-    yAxis: {
-        title: {
-            text: 'Respondents',
-            style: {
-              color: 'black',
-              fontWeight: 'bold'
-            }
-        }
-    },
-    plotOptions: {
-        area: {
-            fillOpacity: 0.5,
-            color: "#14195c"
-        },
-        dataLabels: {
-          enabled: false,
-          style: {
-            color: 'black',
-            fontWeight: 'bold'
-          }
-        }
-    },
-    series: [{
-        name: 'Responses',
-        data: [65, 119, 141, 184, 158, 119, 93, 58, 26, 12, 14, 7, 4, 4, 2, 0, 1, 0, 0, 0, 2]
-    }]
+    dataLabels: {
+      enabled: false,
+      style: {
+        color: 'black',
+        fontWeight: 'bold'
+      }
+    }
+  },
+  series: [{
+    name: 'Size of the Senior Class',
+    data: [260,
+282,
+381,
+383,
+393,
+336,
+375,
+362,
+392,
+419,
+389,
+400,
+417,
+405,
+397,
+388,
+399,
+415,
+405,
+376,
+377,
+365,
+372,
+375,
+355,
+364,
+363,
+347,
+334,
+311,
+313,
+314,
+316,
+317,
+318,
+328,
+307,
+332,
+270,
+338,
+315,
+327,
+339,
+343,
+324,
+319,
+339,
+327,
+305]
+  }]
 });
